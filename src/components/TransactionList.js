29aa092b770fc8ebd5f4c359322940252bfcae13
Deshan-salitha <71576127+Deshan-salitha@users.noreply.github.com>
2022-06-10 10:42:59 +0530

    Customer transaction

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -55,7 +55,7 @@ const TransactionList = () => {
                     <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                         {blog.type}
                     </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    {/* <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                         <button>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -70,7 +70,7 @@ const TransactionList = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                             </svg>
                         </button>
-                    </td>
+                    </td> */}
                 </tr>
             ))}
 
